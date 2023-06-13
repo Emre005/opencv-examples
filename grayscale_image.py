e2cedaa3dd10d51cb0007758db514dbca53371f6
Emre <emre.erol@hotmail.com>
2023-06-13 15:03:11 +0300

    Create grayscale_image.py

---
+++ grayscale_image.py
@@ -0,0 +1,16 @@
+import cv2
+
+# Görüntüyü yükleyin
+img = cv2.imread('dosya_yolu/dosya_adi.jpg', cv2.IMREAD_COLOR)
+
+# Görüntüyü gri tonlamaya çevirin
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Gri tonlamalı görüntüyü gösterin
+cv2.imshow('Gri Görüntü', gray)
+
+# Bir tuşa basana kadar bekleyin
+cv2.waitKey(0)
+
+# Tüm pencereleri kapatın
+cv2.destroyAllWindows()
